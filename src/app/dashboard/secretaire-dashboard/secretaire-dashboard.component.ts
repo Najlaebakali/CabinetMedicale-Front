78b6fc7f912c1942cecebeec6993eef990820df4
Najlaebk <bakalinajlae83@gmail.com>
2024-12-22 20:27:28 +0100

    Résolution des conflits2

--- src/app/dashboard/secretaire-dashboard/secretaire-dashboard.component.ts
+++ src/app/dashboard/secretaire-dashboard/secretaire-dashboard.component.ts
@@ -1,13 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-<<<<<<< HEAD
+
 import { AuthService } from '../../shared/services/auth.service';
 
-import { Router } from '@angular/router';
-=======
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
->>>>>>> 53cf1840ca973507863d2d81b7e86807c411c0e9
 
 @Component({
   selector: 'app-secretaire-dashboard',
@@ -17,15 +14,14 @@ import { RouterModule } from '@angular/router';
   styleUrl: './secretaire-dashboard.component.css'
 })
 export class SecretaireDashboardComponent {
-<<<<<<< HEAD
+
 
   
 
-=======
+
   status = false;
   addToggle()
 {
   this.status = !this.status;       
 }
->>>>>>> 53cf1840ca973507863d2d81b7e86807c411c0e9
 }
